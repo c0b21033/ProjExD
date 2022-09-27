54aa168e5096cb2e987eaca91729829f3b671a9f
Kota <c0b2103371@edu.teu.ac.jp>
2022-09-27 15:42:00 +0900

    正解の判定部分を改善　問題数4

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -2,18 +2,16 @@ import random
 from tokenize import Number
 def shutudai(): #問題をランダムに出題する関数
     global answers #kaito関数で使用するので
-    questions = ["鮪の読みを答えよ", "魑魅魍魎の読みを答えよ", "亜米利加の読みを答えよ"] #問題のリスト
-    answers = [["まぐろ", "マグロ"], ["ちみもうりょう", "チミモウリョウ"], ["あめりか", "アメリカ"]] #答えのリスト
-    number = random.randint(0, 2) #問題と答えの番号をランダムに決定し、変数に保存
+    questions = ["鮪の読みを答えよ", "魑魅魍魎の読みを答えよ", "亜米利加の読みを答えよ", "闘球の読みを答えよ"] #問題のリスト
+    answers = [["まぐろ", "マグロ"], ["ちみもうりょう", "チミモウリョウ"], ["あめりか", "アメリカ"], ["らぐびー", "ラグビー"]] #答えのリスト
+    number = random.randint(0, 3) #問題と答えの番号をランダムに決定し、変数に保存
     print(questions[number]) #問題の出題
     return number #問題と答えの番号を返す
 
 def kaito(number): #ユーザーの解答を受け付け結果を返す関数
     ans = input("ひらがなかカタカナで答えてね：") #解答を受け付ける
-    for i in answers[number]: #ユーザーの解答が答えのリストにあるか調べる
-        if ans == i: #正解の場合
-            result = "正解！"
-            break
+    if ans in answers[number]: #正解の場合
+        result = "正解"
     else: #不正解の場合
         result = "不正解"
     return result #結果を返す
