8912f1b2a8a002113a2ea0be040336233aab7681
Kota <c0b2103371@edu.teu.ac.jp>
2022-09-27 15:30:26 +0900

    とりあえず動く　問題数3

---
+++ ex01/quiz.py
@@ -0,0 +1,26 @@
+import random
+from tokenize import Number
+def shutudai(): #問題をランダムに出題する関数
+    global answers #kaito関数で使用するので
+    questions = ["鮪の読みを答えよ", "魑魅魍魎の読みを答えよ", "亜米利加の読みを答えよ"] #問題のリスト
+    answers = [["まぐろ", "マグロ"], ["ちみもうりょう", "チミモウリョウ"], ["あめりか", "アメリカ"]] #答えのリスト
+    number = random.randint(0, 2) #問題と答えの番号をランダムに決定し、変数に保存
+    print(questions[number]) #問題の出題
+    return number #問題と答えの番号を返す
+
+def kaito(number): #ユーザーの解答を受け付け結果を返す関数
+    ans = input("ひらがなかカタカナで答えてね：") #解答を受け付ける
+    for i in answers[number]: #ユーザーの解答が答えのリストにあるか調べる
+        if ans == i: #正解の場合
+            result = "正解！"
+            break
+    else: #不正解の場合
+        result = "不正解"
+    return result #結果を返す
+
+def main(): #クイズゲームを行う関数
+    number = shutudai() #numberに問題と解答の番号を代入しながらクイズを出題する
+    result = kaito(number) #resultに結果を代入
+    print(result) #結果を表示する
+
+main() #関数の実行
