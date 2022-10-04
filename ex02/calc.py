9bfea99cc151bae102263ee06804056d292a72b8
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-04 14:56:53 +0900

    6 まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,6 +6,7 @@ def button_click(event):
     btn = event.widget #どのボタンが押されたか
     txt = btn["text"] #押されたボタンのtext属性を返す　（クラス）
     entry.insert(tk.END, txt)
+
 root = tk.Tk()
 
 root.geometry("300x600")
@@ -14,10 +15,15 @@ entry.grid(row=0, column=0,  columnspan=3)
 
 x = 0
 y = 0
-for n in range(9, -1, -1):
-    button = tk.Button(root, text=f"{n}", font=("Times New Roman", 30),  width=4, height=2)
-    button.bind("<1>", button_click)
-    button.grid(row = y+1,column = x)
+for n in range(9, -2, -1):
+    if n == -1:
+        button = tk.Button(root, text=f"+", font=("Times New Roman", 30),  width=4, height=2)
+        button.bind("<1>", button_click)
+        button.grid(row = y+1, column = x)
+    else:
+        button = tk.Button(root, text=f"{n}", font=("Times New Roman", 30),  width=4, height=2)
+        button.bind("<1>", button_click)
+        button.grid(row = y+1,column = x)
     x += 1
     if x % 3 == 0:
         x = 0
