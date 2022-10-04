bd33dd40f8c11cdc61f4a702da858f0fc2619a36
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-04 14:43:33 +0900

    4 まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,19 +1,23 @@
 from re import X
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right
 def button_click(event):
     btn = event.widget #どのボタンが押されたか
     txt = btn["text"] #押されたボタンのtext属性を返す　（クラス）
     tkm.showinfo(txt, f"{txt}ボタンが押されました")
 root = tk.Tk()
 
-root.geometry("300x500")
+root.geometry("300x600")
+entry = tk.Entry(root, width=10, font=("Times New Roman", 40), justify="right")
+entry.grid(row=0, column=0,  columnspan=3)
+
 x = 0
 y = 0
 for n in range(9, -1, -1):
     button = tk.Button(root, text=f"{n}", font=("Times New Roman", 30),  width=4, height=2)
     button.bind("<1>", button_click)
-    button.grid(row = y,column = x)
+    button.grid(row = y+1,column = x)
     x += 1
     if x % 3 == 0:
         x = 0
