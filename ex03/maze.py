98577706aa45ba301453fb170f68c41948dc2fd7
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-18 14:30:07 +0900

    keyupの実装

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,6 +8,10 @@ def key_down(event):
     key = event.keysym
     print(key)
 
+def key_up(event):
+    global key
+    key = ""
+
 cx, cy = 300, 400
 c = tk.Canvas(root,width = 1500, height =900, bg = "black")
 c.pack()
@@ -15,4 +19,5 @@ tori = tk.PhotoImage(file="ex03/fig/5.png")
 c.create_image(cx, cy, image=tori, tag="tori")
 key = ""
 root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease>", key_up)
 root.mainloop()
