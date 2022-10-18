745ee847e571a8e1bdaf6044605a571269a3fe0c
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-18 14:16:10 +0900

    画像の貼り付け

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,7 +3,9 @@ import tkinter as tk
 root = tk.Tk()
 root.title("迷路ゲーム")
 
+cx, cy = 300, 400
 c = tk.Canvas(root,width = 1500, height =900, bg = "black")
 c.pack()
-
+tori = tk.PhotoImage(file="ex03/fig/5.png")
+c.create_image(cx, cy, image=tori, tag="tori")
 root.mainloop()
