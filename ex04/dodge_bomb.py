1db8440783eaa35fc01ce01a6bf8e80da33ff486
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-25 14:27:23 +0900

    練習3まで

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,12 +9,19 @@ def main():
     bg_rct = bg_sfc.get_rect()
     clock = pg.time.Clock()
 
+    tori_sfc = pg.image.load("ex04/fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
-        pg.display.update()
         clock.tick(1000)
         for event in pg.event.get():
             if event.type == pg.QUIT:return
+
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        pg.display.update()
         
 if __name__ == "__main__":
     pg.init()
