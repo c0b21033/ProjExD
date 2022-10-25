e40fb341a3c6d09e534ddf42b15afd6482596d16
Kota <c0b2103371@edu.teu.ac.jp>
2022-10-25 14:52:49 +0900

    練習5まで

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,10 +1,10 @@
 import pygame as pg
 import sys
-
+from random import randint
 def main():
     pg.display.set_caption("逃げろこうかとん")
     scrn_sfc = pg.display.set_mode((1200, 600))
-
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
     clock = pg.time.Clock()
@@ -14,6 +14,11 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    bomb_sfc = pg.Surface((20, 20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx, bomb_rct.centery = randint(0, scrn_rct.width), randint(0, scrn_rct.height)
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
         clock.tick(1000)
@@ -29,6 +34,7 @@ def main():
         if key_states[pg.K_RIGHT]:
             tori_rct.centerx+=1
         scrn_sfc.blit(tori_sfc, tori_rct)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         pg.display.update()
         
 if __name__ == "__main__":
