ff373edac700432bf0083dbf4b983089cc5a2156
Kota <c0b2103371@edu.teu.ac.jp>
2022-11-01 17:26:18 +0900

    READMEの追加

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -2,7 +2,9 @@ import pygame as pg
 import sys
 from random import randint
 
+
 class Screen:
+
     def __init__(self, title, scr_size, background):
         self.title = title
         pg.display.set_caption(title)
@@ -14,7 +16,9 @@ class Screen:
     def blit(self):
         return self.sfc.blit(self.back, self.back_rct)
 
+
 class Bird:
+
     key_delta = {
     pg.K_UP:    [0, -1],
     pg.K_DOWN:  [0, +1],
@@ -26,15 +30,18 @@ class Bird:
         self.sfc = pg.transform.rotozoom(self.sfc, 0, zoom)
         self.rct = self.sfc.get_rect()
         self.rct.center = pos
-        self.life = hp
-        self.weapon = pg.image.load(weapon)
+        self.life = hp #こうかとんの体力
+        #武器の画像の読み込みからrect取得まで
+        self.weapon = pg.image.load(weapon) 
         self.weapon = pg.transform.rotozoom(self.weapon, 0, weapon_zoom)
-        self.weapon_rct = self.sfc.get_rect()
+        self.weapon_rct = self.sfc.get_rect() 
 
+    #こうかとんの体力をバーとして表示する関数
     def draw_life(self, screen):
         fonto = pg.font.Font(None, 80)
         txt = fonto.render("LIFE", True, "BLACK")
         screen.sfc.blit(txt, (930, 10))
+        #HPをバーとして表示
         pg.draw.rect(screen.sfc, (0, 255, 0), (1080, 20, self.life, 30))
 
     def blit(self, sfc:Screen):
@@ -46,28 +53,32 @@ class Bird:
             if key_states[key]:
                 self.rct.centerx += delta[0]
                 self.rct.centery += delta[1]
-                # 練習7
                 if check_bound(self.rct, screen.rct) != (+1, +1):
                     self.rct.centerx -= delta[0]
                     self.rct.centery -= delta[1]
-        self.draw_life(screen)
+        self.draw_life(screen) #HPの描写
+        #武器の座標をこうかとんの座標を基に設定することで武器を動かす。
         self.weapon_rct.center = (self.rct.centerx+50, self.rct.centery-40)
         screen.sfc.blit(self.sfc, self.rct)
+        #武器の描写
         screen.sfc.blit(self.weapon, self.weapon_rct)
         
 
 class Bomb(pg.sprite.Sprite):
+
     def __init__(self, color, radius, speed, screen, file, zoom):
         self.sfc = pg.image.load(file)
         self.rct = self.sfc.get_rect()
-        pg.draw.circle(self.sfc, color, (self.rct.width//2, self.rct.height//2), radius) # 爆弾用の円を描く
+        #円を大きくした際にちゃんと円が描写されるように変更
+        pg.draw.circle(self.sfc, color, (self.rct.width//2, self.rct.height//2),radius)
         self.sfc = pg.transform.rotozoom(self.sfc, 0, zoom)
-        self.sfc.set_colorkey((255, 255, 255)) # 四隅の黒い部分を透過させる
+        self.sfc.set_colorkey((255, 255, 255)) # 四隅の部分を透過させる
         self.rct = self.sfc.get_rect()
+        #描写位置をこのようにすることで円が画面の端にめり込んで生成されるのを防ぐ
         self.rct.centerx = randint(self.rct.width, screen.rct.width//2-self.rct.width)
         self.rct.centery = randint(self.rct.height, screen.rct.height-self.rct.height)
         self.vx, self.vy = +speed[0], +speed[1] # 練習6
-        self.flag = True
+        self.flag = True #敵が生きているかどうか
 
     def blit(self, screen):
         return screen.sfc.blit(self.sfc, self.rct)
@@ -78,6 +89,19 @@ class Bomb(pg.sprite.Sprite):
         self.vy *= tate
         self.rct.move_ip(self.vx, self.vy)
         screen.sfc.blit(self.sfc, self.rct)
+
+
+class Item: #アイテムを作成するクラス
+    def __init__(self, file, pos, zoom):
+        self.sfc = pg.image.load(file)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, zoom)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = pos
+        self.sfc.set_colorkey((255, 255, 255))
+    
+    def blit(self, screen):
+        return screen.sfc.blit(self.sfc, self.rct)
+
 
 def check_bound(obj_rct, scr_rct):
     """
@@ -92,16 +116,6 @@ def check_bound(obj_rct, scr_rct):
         tate = -1
     return yoko, tate
 
-class Item:
-    def __init__(self, file, pos, zoom):
-        self.sfc = pg.image.load(file)
-        self.sfc = pg.transform.rotozoom(self.sfc, 0, zoom)
-        self.rct = self.sfc.get_rect()
-        self.rct.center = pos
-        self.sfc.set_colorkey((255, 255, 255))
-    
-    def blit(self, screen):
-        return screen.sfc.blit(self.sfc, self.rct)
 
 def main():
     x, y = 0, 0
@@ -114,50 +128,54 @@ def main():
 
     while True:
         screen.blit()
-        
+
         for event in pg.event.get(): 
             if event.type == pg.QUIT:
                 return
 
         tori.update(screen)
+        #敵が生きていれば
         if bomb.flag == True:
             bomb.update(screen)
         if bomb2.flag == True:
             bomb2.update(screen)
 
-        #if tori.shiled:
-
-
+        #敵と接触したかつ、敵が生きていれば
         if tori.rct.colliderect(bomb.rct) and bomb.flag == True:
             tori.life -= 1  
-        if tori.rct.colliderect(bomb2.rct) and bomb2.flag == True: # こうかとんrctが爆弾rctと重なったら
+        if tori.rct.colliderect(bomb2.rct) and bomb2.flag == True:
             tori.life -= 1
 
+        #こうかとんのHPが０になったら終わり
         if tori.life == 0:
             return
 
+        #時刻の取得
         now = pg.time.get_ticks()
+        #一定間隔でアイテムを移動させる
         if now % 1000 == 0:
             x = randint(screen.rct.left, screen.rct.right)
             y = randint(screen.rct.top, screen.rct.bottom)
             item = Item("ex05/fig/Item.png", (x, y), 0.5)
         item.blit(screen)
 
+        #アイテムとぶつかったら
         if tori.rct.colliderect(item.rct):
             print("Itemとぶつかってしまいました")
             clock.tick(2)
             return
 
+        #武器が敵と当たったら、敵の状態を死亡にする
         if tori.weapon_rct.colliderect(bomb.rct):
             bomb.flag = False
 
         if tori.weapon_rct.colliderect(bomb2.rct):
             bomb2.flag = False
 
-
         pg.display.update() #練習2
         clock.tick(1000)
-        
+
+
 if __name__ == "__main__":
     pg.init() # 初期化
     main()    # ゲームの本体
